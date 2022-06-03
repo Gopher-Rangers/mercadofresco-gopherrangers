067025322d5a891d4453045eda888b378ce46998
anolivei <anolivei@student.42sp.org.br>
2022-06-03 15:33:17 -0300

    First of Requirement Product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/Gopher-Rangers/mercadofresco-gopherrangers/tree/main/cmd/server/handler/products"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("failed to load .env")
+	}
+	
 	gin := gin.Default()
 
 	baseRoute := gin.Group("/api/v1/")
@@ -10,12 +21,11 @@ func main() {
 		productsRouterGroup := baseRoute.Group("/products")
 		{
 			productsRouterGroup.POST("/", productHandler.Save())
-			productsRouterGroup.GET("/", productHandler.GetAll())
-			productsRouterGroup.GET("/:id", productHandler.GetById())
-			productsRouterGroup.PUT("/:id", productHandler.Update())
-			productsRouterGroup.DELETE("/:id", productHandler.Delete())
-			productsRouterGroup.PATCH("/:id", productHandler.PatchNamePrice())
+			//productsRouterGroup.GET("/", productHandler.GetAll())
+			//productsRouterGroup.GET("/:id", productHandler.GetById())
+			//productsRouterGroup.PUT("/:id", productHandler.Update())
+			//productsRouterGroup.DELETE("/:id", productHandler.Delete())
+			//productsRouterGroup.PATCH("/:id", productHandler.PatchNamePrice())
 		}
 	}
-
 }

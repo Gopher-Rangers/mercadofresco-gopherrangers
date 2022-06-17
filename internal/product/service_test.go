f74d58b60e3f303465922fd3f431c8249a397aaa
anolivei <anolivei@student.42sp.org.br>
2022-06-17 15:44:48 -0300

    first test and workflow

--- internal/product/service_test.go
+++ internal/product/service_test.go
@@ -1,1 +1,23 @@
 package products_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Gopher-Rangers/mercadofresco-gopherrangers/internal/product"
+	"github.com/Gopher-Rangers/mercadofresco-gopherrangers/internal/product/mocks"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDelete(t *testing.T) {
+	t.Run("delete_non_existent", func(t *testing.T) {
+		mockRepository := mocks.NewRepository(t)
+		mockRepository.On("Delete", 9).Return(fmt.Errorf("produto não encontrado"))
+
+		service := products.NewService(mockRepository)
+		err := service.Delete(9)
+
+		assert.Equal(t, fmt.Errorf("produto não encontrado"), err)
+	})
+}

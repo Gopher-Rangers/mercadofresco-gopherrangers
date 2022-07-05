18599f511e89d44c018a108667a6a4248bf3b842
Matheus Simas <matheussimasalves@gmail.com>
2022-07-05 19:02:55 -0300

    implements create_conflict test

--- cmd/server/handlers/employees_test.go
+++ cmd/server/handlers/employees_test.go
@@ -91,17 +91,17 @@ func TestEmployeeCreate(t *testing.T) {
 		emps := createEmployeesArray()
 		expected := `{"id": 1, "card_number_id": 117899, "first_name": "Jose", "last_name": "Neves", "warehouse_id": 456521}`
 		req, rr := createEmployeeRequestTest(http.MethodPost, URL_EMPLOYEES, expected)
-
-		mockService.On("GetAll").Return(emps, nil)
-		mockService.On("Create", emps[0].CardNumber, emps[0].FirstName, emps[0].LastName, emps[0].WareHouseID).Return(emps[0], http.StatusConflict)
-
+		mockService.On("GetAll").Return([]employee.Employee{})
+		mockService.On("Create", emps[0].CardNumber, emps[0].FirstName, emps[0].LastName, emps[0].WareHouseID).Return(employee.Employee{}, fmt.Errorf("funcionário com cartão nº: 117899 já existe no banco de dados"))
 		employeesRouterGroup.POST("/", handlerEmployee.Create())
 		server.ServeHTTP(rr, req)
+
 		resp := responseOne{}
 		json.Unmarshal(rr.Body.Bytes(), &resp)
 
+		assert.Equal(t, http.StatusConflict, rr.Code, resp.Code)
 		assert.Equal(t, employee.Employee{}, resp.Data)
-		assert.Equal(t, http.StatusConflict, resp.Code, resp.Error)
+		assert.Equal(t, resp.Error, "funcionário com cartão nº: 117899 já existe no banco de dados")
 	})
 }
 

c6407ee436d4fc9c12399a6f7284ae83fddf6464
leandropiassetta <leandropiassetta@gmail.com>
2022-07-08 20:48:24 -0300

    feat: createCarry

--- internal/carry/adapters/carry_mysql_repository.go
+++ internal/carry/adapters/carry_mysql_repository.go
@@ -2,13 +2,14 @@ package adapters
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/Gopher-Rangers/mercadofresco-gopherrangers/internal/carry/domain"
 	"github.com/Gopher-Rangers/mercadofresco-gopherrangers/internal/carry/usecases"
 )
 
 const (
-	queryCreateCarry = "INSERT INTO carriers () VALUES (?, ?, ?, ?, ?)"
+	queryCreateCarry = "INSERT INTO carriers (cid, name, address, telephone, locality_id) VALUES (?, ?, ?, ?, ?)"
 )
 
 type mysqlCarryRepository struct {
@@ -19,13 +20,40 @@ func NewMySqlCarryRepository(db *sql.DB) usecases.RepositoryCarry {
 	return &mysqlCarryRepository{db: db}
 }
 
-func CreateCarry(r *mysqlCarryRepository) (domain.Carry, error) {
-	stmt, err := r.db.Prepare()
+func (r *mysqlCarryRepository) CreateCarry(carry domain.Carry) (domain.Carry, error) {
+	stmt, err := r.db.Prepare(queryGetAllCarriesLocalityByID)
+
+	if err != nil {
+		return domain.Carry{}, fmt.Errorf("erro ao preparar a query")
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(carry.Cid, carry.Name, carry.Address, carry.Telephone, carry.LocalityID)
+
+	if err != nil {
+		return domain.Carry{}, fmt.Errorf("erro ao executar a query")
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return domain.Carry{}, fmt.Errorf("falha ao obter o id no banco de dados")
+	}
+
+	return domain.Carry{
+		ID:         int(id),
+		Cid:        carry.Cid,
+		Name:       carry.Name,
+		Telephone:  carry.Telephone,
+		LocalityID: carry.LocalityID,
+	}, nil
+
 }
 
-func GetCaryPerLocality(r mysqlCarryRepository) (domain.Carry, error) {
+func (r mysqlCarryRepository) GetCaryPerLocality(id int) (domain.Carry, error) {
 	return domain.Carry{}, nil
 }
-func GetCarryByCid(r mysqlCarryRepository) (domain.Carry, error) {
+func (r mysqlCarryRepository) GetCarryByCid(cid string) (domain.Carry, error) {
 	return domain.Carry{}, nil
 }
--- internal/carry/usecases/service_carry.go
+++ internal/carry/usecases/service_carry.go
@@ -1,11 +1,14 @@
 package usecases
 
-import "github.com/Gopher-Rangers/mercadofresco-gopherrangers/internal/carry/domain"
+import (
+	"fmt"
+
+	"github.com/Gopher-Rangers/mercadofresco-gopherrangers/internal/carry/domain"
+)
 
 type ServiceCarry interface {
 	CreateCarry(carry domain.Carry) (domain.Carry, error)
 	GetCaryPerLocality(id int) (domain.Carry, error)
-	GetCarryByCid(cid string) (domain.Carry, error)
 }
 
 type serviceCarry struct {
@@ -14,4 +17,25 @@ type serviceCarry struct {
 
 func NewServiceCarry(r ServiceCarry) ServiceCarry {
 	return &service{r}
+}
+
+func (s *serviceCarry) CreateCarry(carry domain.Carry) (domain.Carry, error) {
+
+	_, err := s.repository.GetCarryByCid(carry.Cid)
+
+	if err == nil {
+		return domain.Carry{}, fmt.Errorf("o `cid` já está em uso")
+	}
+
+	carry, err = s.repository.CreateCarry(carry)
+
+	if err != nil {
+		return domain.Carry{}, err
+	}
+
+	return carry, nil
+
+}
+func (s serviceCarry) GetCaryPerLocality(id int) (domain.Carry, error) {
+	return domain.Carry{}, nil
 }

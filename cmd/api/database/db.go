d4fad72f3b7765a06c68c777e3feba40ef26b13b
anolivei <anolivei@student.42sp.org.br>
2022-07-14 18:15:03 -0300

    some fixes to fury

--- cmd/api/database/db.go
+++ cmd/api/database/db.go
@@ -5,20 +5,21 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/mercadolibre/go-meli-toolkit/gomelipass"
+	//"github.com/mercadolibre/go-meli-toolkit/gomelipass"
 )
 
 var conn *sql.DB
 
 func GetInstance() *sql.DB {
 	if conn == nil {
+		var dbHost string
+		var connectionString string
 		//linhas 17 a 21: trocar x pelo seu numero de feature ou copiar e colar direto dos snippets
-		dbUsername := "bgow1s48x_WPROD"
-		dbPassword := gomelipass.GetEnv("DB_MYSQL_DESAENV05_BGOW1S48x_BGOW1S48x_WPROD")
-		dbHost := gomelipass.GetEnv("DB_MYSQL_DESAENV05_BGOW1S48x_BGOW1S48x_ENDPOINT")
-		dbName := "bgow1s48x"
-		connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", dbUsername, dbPassword, dbHost, dbName)
+		// dbUsername := "bgow1s48x_WPROD"
+		// dbPassword := gomelipass.GetEnv("DB_MYSQL_DESAENV05_BGOW1S48x_BGOW1S48x_WPROD")
+		// dbHost = gomelipass.GetEnv("DB_MYSQL_DESAENV05_BGOW1S48x_BGOW1S48x_ENDPOINT")
+		// dbName := "bgow1s48x"
+		// connectionString = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", dbUsername, dbPassword, dbHost, dbName)
 		if dbHost == "" {
 			connectionString = fmt.Sprintf(
 				"%s:%s@tcp(%s:%s)/%s?charset=utf8",
--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/go-playground/validator/v10 v10.11.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.4.0
-	github.com/mercadolibre/go-meli-toolkit v0.0.0-20220608183925-dc6b17665e52
 	github.com/stretchr/testify v1.8.0
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe
 	github.com/swaggo/gin-swagger v1.5.1
@@ -30,6 +29,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
